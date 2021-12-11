c57a640a8eb31b681ea0853f4ef075a078dddf6e
Minh Nhat <leminhnhat2406@gmail.com>
2021-12-11 20:00:40 +0700

    xác thực người dùng (đk, đn, đx)

---
+++ src/main/java/com/qltc/quanlitieccuoi/configuration/SpringSecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.qltc.quanlitieccuoi.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+	// Do nothing
+
+}
